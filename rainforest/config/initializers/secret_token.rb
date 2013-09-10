eeccc31cbede7fc2798929c5d8ef7da7fbaf332f
amber-h <amber.houle3@gmail.com>
2013-09-09 20:09:13 -0400

    first commit

---
+++ rainforest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainforest::Application.config.secret_token = '23bffaa5f54a24cfeea2eff1c2abafdef7f68b096fa0b991144063655f99f53884e95e9405eee4ef790d28042de9db86681503fa920287d82d3311ca91f75d4a'
