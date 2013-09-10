30ae38fac34a8ca203071834b14e9e54285247f7
amber-h <amber.houle3@gmail.com>
2013-09-10 14:55:05 -0400

    added the views for Product model

--- rainforest/db/seeds.rb
+++ rainforest/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!(
+    :description  => "Chocolate Bar",
+    :name => "KitKat",
+    :price_in_cents => "139"
+  )
